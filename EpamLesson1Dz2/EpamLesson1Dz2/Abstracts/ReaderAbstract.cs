3e6390de5bb20d933013bceec86b8037645a63d2
Vit_vi <vitalik99968@gmail.com>
2019-08-13 15:49:38 +0300

    Creating

---
+++ EpamLesson1Dz2/EpamLesson1Dz2/Abstracts/ReaderAbstract.cs
@@ -0,0 +1,18 @@
+﻿using EpamLesson1Dz2.Interfaces;
+using System.Collections.Generic;
+
+namespace EpamLesson1Dz2.Abstracts
+{
+    abstract class ReaderAbstract : IReader
+    {
+        // Contains everyone person in story.
+        public List<Person> People { get; protected set; }
+
+        public abstract void StartReading(IWriter writer);
+
+        protected virtual void _GetExeption()
+        {
+            throw new PersonExeption("Напали пришельцы.","Все были уничтожены.");
+        }
+    }
+}
